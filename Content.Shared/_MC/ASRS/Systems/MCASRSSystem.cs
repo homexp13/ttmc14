d21eb7ac94f4f7f70e394459cea338ca94d408a6
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-10-15 13:21:19 +1000

    wip

---
+++ Content.Shared/_MC/ASRS/Systems/MCASRSSystem.cs
@@ -0,0 +1,15 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._MC.ASRS.Systems;
+
+public sealed class MCASRSSystem : MCEntitySystemSingleton<MCASRSSingletonComponent>
+{
+
+}
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class MCASRSSingletonComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public int Points;
+}
---
+++ Content.Shared/_MC/Miners/Components/MCMinerComponent.cs
@@ -0,0 +1,37 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._MC.Miners.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class MCMinerComponent : Component
+{
+    /// <summary>
+    /// The mineral type that's produced.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public int MineralValue = 150;
+
+    /// <summary>
+    /// How many sheets of material we have stored.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public int MineralStored;
+
+    /// <summary>
+    /// How many sheets of material we can store.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public int MineralStorage = 8;
+
+    /// <summary>
+    /// How many times we need for a resource to be created.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public TimeSpan MineralProductionTime = TimeSpan.FromSeconds(140);
+
+    /// <summary>
+    /// Applies the actual bonus points for the dropship for each sale.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public int DropshipBonus = 15;
+}
