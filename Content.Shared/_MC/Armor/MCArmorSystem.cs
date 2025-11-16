e1b4fe4c2e2335e62571984dbc23d8c23d591cd0
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-11-16 22:46:05 +1000

    feat: armor examine

--- Content.Shared/_MC/Armor/MCArmorSystem.cs
+++ Content.Shared/_MC/Armor/MCArmorSystem.cs
@@ -1,12 +1,16 @@
 ﻿using Content.Shared._MC.Xeno.Sunder;
 using Content.Shared._RMC14.Armor;
 using Content.Shared._RMC14.Weapons.Ranged;
+using Content.Shared._RMC14.Xenonids;
 using Content.Shared.Damage;
+using Content.Shared.Examine;
 using Content.Shared.Explosion;
 using Content.Shared.Inventory;
 using Content.Shared.Tag;
+using Content.Shared.Verbs;
 using Content.Shared.Weapons.Melee;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
 
 namespace Content.Shared._MC.Armor;
 
@@ -21,9 +25,11 @@ public sealed class MCArmorSystem : EntitySystem
     private static readonly ProtoId<TagPrototype> TagFire = "MCDamageFire";
     private static readonly ProtoId<TagPrototype> TagAcid = "MCDamageAcid";
 
-    [Dependency] private readonly TagSystem _tag = default!;
-    [Dependency] private readonly InventorySystem _inventory = default!;
-    [Dependency] private readonly MCXenoSunderSystem _xenoSunder = default!;
+    [Dependency] private readonly TagSystem _tag = null!;
+    [Dependency] private readonly InventorySystem _inventory = null!;
+    [Dependency] private readonly ExamineSystemShared _examine = null!;
+
+    [Dependency] private readonly MCXenoSunderSystem _mcXenoSunder = null!;
 
     public override void Initialize()
     {
@@ -35,6 +41,7 @@ public sealed class MCArmorSystem : EntitySystem
         SubscribeLocalEvent<MCArmorComponent, InventoryRelayedEvent<GetExplosionResistanceEvent>>(OnGetExplosionResistanceRelayed);
 
         SubscribeLocalEvent<MCArmorComponent, DamageModifyEvent>(OnDamageModify);
+        SubscribeLocalEvent<MCArmorComponent, GetVerbsEvent<ExamineVerb>>(OnArmorVerbExamine);
 
         SubscribeLocalEvent<InventoryComponent, MCArmorGetEvent>(_inventory.RelayEvent);
     }
@@ -65,12 +72,12 @@ public sealed class MCArmorSystem : EntitySystem
 
     private void OnGetExplosionResistance(Entity<MCArmorComponent> entity, ref GetExplosionResistanceEvent args)
     {
-        args.DamageCoefficient *= ArmorToValue(entity.Comp.Bomb, 0, _xenoSunder.GetSunder(entity.Owner));
+        args.DamageCoefficient *= ArmorToValue(entity.Comp.Bomb, 0, _mcXenoSunder.GetSunder(entity.Owner));
     }
 
     private void OnGetExplosionResistanceRelayed(Entity<MCArmorComponent> entity, ref InventoryRelayedEvent<GetExplosionResistanceEvent> args)
     {
-        args.Args.DamageCoefficient *= ArmorToValue(entity.Comp.Bomb, 0, _xenoSunder.GetSunder(entity.Owner));
+        args.Args.DamageCoefficient *= ArmorToValue(entity.Comp.Bomb, 0, _mcXenoSunder.GetSunder(entity.Owner));
     }
 
     private void OnDamageModify(Entity<MCArmorComponent> entity, ref DamageModifyEvent args)
@@ -83,7 +90,7 @@ public sealed class MCArmorSystem : EntitySystem
         var ev = new MCArmorGetEvent(SlotFlags.OUTERCLOTHING | SlotFlags.INNERCLOTHING);
         RaiseLocalEvent(entityUid, ref ev);
 
-        var sunderModifier = _xenoSunder.GetSunder(entityUid);
+        var sunderModifier = _mcXenoSunder.GetSunder(entityUid);
 
         if (args.Tool is not { } tool)
             return;
@@ -116,5 +123,54 @@ public sealed class MCArmorSystem : EntitySystem
     private static float ArmorToValue(int armor, int penetration, float sunder)
     {
         return Math.Clamp((100 - armor * sunder + penetration) * 0.01f, 0, 1);
+    }
+
+    private void OnArmorVerbExamine(Entity<MCArmorComponent> entity, ref GetVerbsEvent<ExamineVerb> args)
+    {
+        if (!args.CanInteract || !args.CanAccess || HasComp<XenoComponent>(entity))
+            return;
+
+        var examineMarkup = GetArmorExamine(entity);
+        _examine.AddDetailedExamineVerb(
+            args,
+            entity,
+            examineMarkup,
+            Loc.GetString("mc-armor-examinable-verb-text"),
+            "/Textures/Interface/Actions/actions_fakemindshield.rsi/icon-on.png",
+            Loc.GetString("mc-armor-examinable-verb-message")
+        );
+    }
+
+    private FormattedMessage GetArmorExamine(MCArmorComponent armorComponent)
+    {
+        var msg = new FormattedMessage();
+        msg.AddMarkupOrThrow(Loc.GetString("mc-armor-examine-title"));
+
+        var armorRatings = new[]
+        {
+            (Loc.GetString("mc-armor-melee"), armorComponent.Melee),
+            (Loc.GetString("mc-armor-bullet"), armorComponent.Bullet),
+            (Loc.GetString("mc-armor-laser"), armorComponent.Laser),
+            (Loc.GetString("mc-armor-energy"), armorComponent.Energy),
+            (Loc.GetString("mc-armor-bomb"), armorComponent.Bomb),
+            (Loc.GetString("mc-armor-bio"), armorComponent.Bio),
+            (Loc.GetString("mc-armor-fire"), armorComponent.Fire),
+            (Loc.GetString("mc-armor-acid"), armorComponent.Acid),
+        };
+
+        foreach (var (text, value) in armorRatings)
+        {
+            if (value == 0)
+                continue;
+
+            msg.PushNewline();
+            msg.AddMarkupOrThrow(Loc.GetString(
+                "mc-armor-examine-armor",
+                ("text", text),
+                ("value", value)
+            ));
+        }
+
+        return msg;
     }
 }
